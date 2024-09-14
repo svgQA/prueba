cd404f247f5a18c7d47c092b1adfedd06621a460
sterling1992 <klever04.ac@gmail.com>
2024-09-14 17:25:02 -0500

    feat: ✨  add modal services

--- src/components/common/navbar/navbar.tsx
+++ src/components/common/navbar/navbar.tsx
@@ -17,7 +17,7 @@ export const Navbar: FunctionComponent<INavbarProps> = ({
       className='flex font-bold px-5 py-4 flex-row justify-between w-full content-center items-center absolute text-white top-0'
     >
       <span>{logo}</span>
-      <ul className='flex flex-row items-center text-center'>
+      <ul className='flex flex-row items-center text-center '>
         {menus.map((menu) => (
           <li key={`navbar-menu-${menu.to}`} className='px-2 relative'>
             {menu.button && onActionHandler ? (
