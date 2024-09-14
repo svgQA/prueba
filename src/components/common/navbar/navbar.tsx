434bb8ca6580b4f21bf73f9e7d35e4d73a89ffa7
sterling1992 <klever04.ac@gmail.com>
2024-09-14 17:51:34 -0500

    feat: ✨ change props on ModalService for de modal as Open forever

--- src/components/common/navbar/navbar.tsx
+++ src/components/common/navbar/navbar.tsx
@@ -35,39 +35,3 @@ export const Navbar: FunctionComponent<INavbarProps> = ({
     </nav>
   );
 };
-
-// import { type FunctionComponent } from 'preact';
-// import { type INavbarProps } from './interface';
-// import { Link } from 'wouter';
-
-// export const Navbar: FunctionComponent<INavbarProps> = ({
-//   id,
-//   name,
-//   menus,
-//   logo,
-//   onActionHandler,
-// }: INavbarProps) => {
-//   return (
-//     <nav
-//       id={id}
-//       name={name}
-//       className='flex font-bold px-5 py-4 flex-row justify-between w-full content-center items-center absolute text-white top-0'
-//     >
-//       <span>{logo}</span>
-//       {/* <span className='px-5 content-center'>{actions}</span> */}
-//       <ul class='flex flex-row items-center text-center'>
-//         {menus.map((menu) => (
-//           <li key={`navbar-menu-${menu.to}`} className='px-2'>
-//             {menu.button && onActionHandler ? (
-//               <button onClick={() => onActionHandler(menu.to)}>
-//                 {menu.label}
-//               </button>
-//             ) : (
-//               <Link to={menu.to}>{menu.label}</Link>
-//             )}
-//           </li>
-//         ))}
-//       </ul>
-//     </nav>
-//   );
-// };
