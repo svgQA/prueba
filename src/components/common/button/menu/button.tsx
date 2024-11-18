98376549be5fa94480ec79881333093573d4015e
Juan Pablo Rodriguez Fernandez <juanpablorodriguezfernandez93@gmail.com>
2024-11-18 01:45:57 +0100

    fix: 🐛 sidebar select icon

--- src/components/common/button/menu/button.tsx
+++ src/components/common/button/menu/button.tsx
@@ -9,13 +9,11 @@ export const ButtonMenu: FunctionComponent<IButtonMenuProps> = ({
 }: IButtonMenuProps) => {
   return (
     <div
-      className={`${small ? 'h-10' : 'h-12'} my-1 text-center overflow-hidden relative cursor-pointer content-end px-1`}
+      className={`${small ? 'h-10' : 'h-12'} my-1 text-center overflow-hidden relative cursor-pointer content-end px-1 hover:text-primary`}
     >
       <span
         name={name}
-        className={
-          'absolute w-full left-0 h-14 -top-1 vox-icon vx-icon-' + icon
-        }
+        className={'absolute w-full left-0 h-14 -top-1 vx-icon vx-icon-' + icon}
       ></span>
       <h6 className={`${small ? 'text-3xs' : 'text-xs'} capitalize`}>
         {label}
