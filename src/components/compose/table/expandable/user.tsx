32365c0671dc201d3289600dfada692b7acb638f
Sixto <sixtorobio@unicauca.edu.co>
2024-12-05 00:18:11 -0500

    fix(data): user expandable

--- src/components/compose/table/expandable/user.tsx
+++ src/components/compose/table/expandable/user.tsx
@@ -7,90 +7,78 @@ export const ExpandableUser: FunctionComponent<IExpandableProps> = ({
   return (
     <div className='p-6 bg-white rounded-lg shadow-md'>
       <div className='grid grid-cols-4 gap-4'>
-        {/* Columna del Empleado */}
-        <div className='space-y-1'>
-          <h4 className='text-gray-600 mb-2 font-medium'>Datos del Empleado</h4>
+        <div className='space-y-2 flex justify-center items-center'>
           <img
             src={row.workerPhoto || '/placeholder-image.jpg'}
             alt='Foto del empleado'
-            className='w-32 h-32 rounded-full mb-4 object-cover hidden [&:not([src="/placeholder-image.jpg"])]:block'
+            className='w-32 h-32 rounded-full  object-cover hidden [&:not([src="/placeholder-image.jpg"])]:block'
           />
-
-          <div className='flex items-center'>
-            <span className='text-gray-600 w-32'>Email:</span>
+        </div>
+        <div className='space-y-2'>
+          <div className=''>
+            <span className='text-gray-600 w-32 mr-2'>Nombre:</span>
+            <span className='text-gray-900'>
+              {row.firstName} {row.lastName}
+            </span>
+          </div>
+          <div className=''>
+            <span className='text-gray-600 w-32 mr-2'>Edad:</span>
+            <span className='text-gray-800'>{row.workerAge}</span>
+          </div>
+          <div className=''>
+            <span className='text-gray-600 w-32 mr-2'>Identificación:</span>
+            <span className='text-gray-800'>{row.personalID}</span>
+          </div>
+          <div className=''>
+            <span className=' text-gray-600 w-32 mr-2'>Email:</span>
             <span className='text-gray-800'>{row.workerEmail}</span>
           </div>
-          <div className='flex items-center'>
-            <span className='text-gray-600 w-32'>Teléfono:</span>
+          <div className=''>
+            <span className='text-gray-600 w-32 mr-2'>Conexión:</span>
+            <span className='text-gray-800'>{row.connection}</span>
+          </div>
+        </div>
+
+        <div className='space-y-2'>
+          <div className=''>
+            <span className='text-gray-600 w-32 mr-2'>Compañia:</span>
+            <span className='text-gray-800'>{row.company}</span>
+          </div>
+          <div className=''>
+            <span className='text-gray-600 w-32 mr-2'>Departamento:</span>
+            <span className='text-gray-800'>{row.department}</span>
+          </div>
+          <div className=''>
+            <span className='text-gray-600 w-32 mr-2'>Ciudad:</span>
+            <span className='text-gray-800'>{row.city}</span>
+          </div>
+          <div className=''>
+            <span className='text-gray-600 w-32 mr-2'>Localización:</span>
+            <span className='text-gray-800'>{row.location}</span>
+          </div>
+          <div className=''>
+            <span className='text-gray-600 w-32 mr-2'>Dirección:</span>
+            <span className='text-gray-800'>{row.address}</span>
+          </div>
+        </div>
+
+        <div className='space-y-2'>
+          <div className=''>
+            <span className='text-gray-600 w-32 mr-2'>Contacto:</span>
             <span className='text-gray-800'>{row.contact}</span>
           </div>
-          <div className='flex items-center'>
-            <span className=' text-gray-600 w-32'>Turno:</span>
+          <div className=''>
+            <span className='text-gray-600 w-32 mr-2'>Turno:</span>
             <span className='text-gray-800'>{row.shift}</span>
           </div>
-        </div>
-
-        {/* Columna del Supervisor */}
-        <div className='space-y-2'>
-          <h4 className=' text-gray-600 mb-2 font-medium'>
-            Datos del Supervisor
-          </h4>
-          <img
-            src={row.supervisorPhoto || '/placeholder-image.jpg'}
-            alt='Foto del supervisor'
-            className='w-32 h-32 rounded-full mb-4 object-cover hidden [&:not([src="/placeholder-image.jpg"])]:block'
-          />
-          <div className='flex items-center'>
-            <span className='text-gray-600 w-32'>Nombre:</span>
-            <span className='text-gray-800'>{row.supervisor}</span>
+          <div className=''>
+            <span className='text-gray-600 w-32 mr-2'>Tipo Novedad:</span>
+            <span className='text-gray-800'>{row.noveltyType}</span>
           </div>
-          <div className='flex items-center'>
-            <span className='text-gray-600 w-32'>Email:</span>
-            <span className='text-gray-800'>{row.supervisorEmail}</span>
+          <div className=''>
+            <span className='text-gray-600 w-32 mr-2'>Fecha Novedad:</span>
+            <span className='text-gray-800'>{row.noveltyDate}</span>
           </div>
-          <div className='flex items-center'>
-            <span className='text-gray-600 w-32'>Teléfono:</span>
-            <span className='text-gray-800'>{row.supervisorPhone}</span>
-          </div>
-        </div>
-
-        {/* Columna del Cliente */}
-        <div className='space-y-2'>
-          <h4 className='text-gray-600 mb-2 font-medium'>Datos del Cliente</h4>
-          {row.clientPhoto && (
-            <img
-              src={row.clientPhoto}
-              alt='Foto del cliente'
-              className='w-32 h-32 rounded-full mb-4 object-cover'
-            />
-          )}
-          <div className='flex items-center'>
-            <span className='text-gray-600 w-32'>Nombre:</span>
-            <span className='text-gray-800'>{row.clientName}</span>
-          </div>
-          <div className='flex items-center'>
-            <span className='text-gray-600 w-32'>Email:</span>
-            <span className='text-gray-800'>{row.clientEmail}</span>
-          </div>
-          <div className='flex items-center'>
-            <span className='text-gray-600 w-32'>Teléfono:</span>
-            <span className='text-gray-800'>{row.clientPhone}</span>
-          </div>
-        </div>
-
-        {/* Columna del Mapa */}
-        <div className='w-full h-48'>
-          <h4 className='text-gray-600 mb-2 font-medium'>
-            Localización Física del Cliente
-          </h4>
-          <iframe
-            title='Ubicación del incidente'
-            src={`https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d!2d${row.location?.split(',')[1] || '0'}!3d${row.location?.split(',')[0] || '0'}!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zM!5e0!3m2!1ses!2s!4v1234567890!5m2!1ses!2s`}
-            className={`w-full h-full rounded-lg border-none ${!row.location ? 'hidden' : ''}`}
-            loading='lazy'
-            allowFullScreen
-            //referrerPolicy='no-referrer-when-downgrade'
-          />
         </div>
       </div>
     </div>
