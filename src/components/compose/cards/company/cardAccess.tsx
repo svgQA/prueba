723931a0b1c74da476a8818c957a4041e346c22b
Sixto <sixtorobio@unicauca.edu.co>
2025-01-30 09:49:22 -0500

    feat(access): fix cards

--- src/components/compose/cards/company/cardAccess.tsx
+++ src/components/compose/cards/company/cardAccess.tsx
@@ -1,5 +1,6 @@
 import { Card } from '@/components/common/card/card';
 import { memo } from 'react';
+import edificioImagen from '../../../../assets/image/edificio.jpg';
 
 type CardProps = {
   title: string;
@@ -12,12 +13,12 @@ type CardProps = {
 export const CardAccess = memo(
   ({ title, subtitle, imageUrl, icon, onEdit }: CardProps) => (
     <Card name='relative flex gap-4 p-4 max-w-md hover:bg-gray-50/50 transition-colors'>
-      <div className='w-128 h-40  rounded-lg overflow-hidden flex-shrink-0 bg-gray-100 flex items-center justify-center'>
-        {imageUrl ? (
-          <img src={''} alt={title} className='w-100 h-full object-cover' />
-        ) : (
-          <div className='w-full h-full bg-gray-100' />
-        )}
+      <div className='w-128 h-40 rounded-lg overflow-hidden flex-shrink-0 bg-gray-100 flex items-center justify-center'>
+        <img
+          src={imageUrl || edificioImagen}
+          alt={title}
+          className='w-full h-full object-cover'
+        />
       </div>
       <div className='flex-1 min-w-0 pr-8'>
         <h3 className='font-semibold text-lg text-gray-900 mb-4 mt-4'>
