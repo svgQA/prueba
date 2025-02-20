e5126d71655fa588e75f7e8160f40976a76a5f5c
Juan Pablo Rodriguez Fernandez <juanpablorodriguezfernandez93@gmail.com>
2025-02-20 10:05:09 +0100

    feat: 🚧 update packages and memo create

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite';
 import preact from '@preact/preset-vite';
 import path from 'path';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact()],
+  plugins: [
+    preact(),
+    visualizer({
+      open: true, // Abre el reporte automáticamente en el navegador
+      filename: 'stats.html', // Nombre del archivo de salida
+      // gzip: true, // Mostrar tamaño con gzip
+      brotliSize: true, // Mostrar tamaño con Brotli
+    }),
+  ],
   base: '/',
   clearScreen: false,
   build: {
@@ -32,6 +41,33 @@ export default defineConfig({
           // if (id.includes('assets')) {
           //   return '@assets-base';
           // }
+          if (id.includes('react-google-maps')) {
+            return '@google-base'; // 148.81 kB
+          }
+          if (id.includes('lodash')) {
+            return '@lodash-base'; // 98.54 kB
+          }
+          if (id.includes('jspdf')) {
+            return '@jspdf-base'; // 358.25 kB
+          }
+          if (id.includes('tanstack')) {
+            return '@tanstack-base'; // 55.80 kB
+          }
+          if (id.includes('qrcode')) {
+            return '@qrcode-base'; // 24.56 kB
+          }
+          if (id.includes('dnd-kit')) {
+            return '@dnd-kit-base'; // 67.50 kB
+          }
+          if (id.includes('aws-amplify')) {
+            return '@aws-amplify-base'; // 413.17 kB
+          }
+          // if (id.includes('chart')) {
+          //   return '@chart-base';
+          // }
+          if (id.includes('final-form')) {
+            return '@final-form-base'; // 30.90 kB
+          }
         },
       },
     },
