db4193a83d47d6f6abf00aa01c86882323c6266d
Juan Pablo Rodriguez Fernandez <juanpablorodriguezfernandez93@gmail.com>
2025-05-02 16:27:49 +0200

    fix: 🐛 update forms view

--- src/components/common/badge/badge.tsx
+++ src/components/common/badge/badge.tsx
@@ -12,7 +12,7 @@ export const Badge: FunctionComponent<IBadgeProps> = ({
   return (
     <span
       className={`
-        text-${size} items-center capitalize px-3 py-0.5 flex justify-between rounded-full border-gray-100 dark:border-gray-700 font-bold text-base
+        text-${size} items-center capitalize px-3 py-0.5 flex justify-between rounded-full border-gray-100 dark:border-gray-700 ${icon ? '' : 'font-bold'} text-base
         ${full ? 'w-full' : 'w-fit'}
         ${borderless ? 'border-none' : 'border'}
         ${!icon ? (status === 'error' ? 'bg-error' : status === 'success' ? 'bg-secondary' : status === 'warning' ? 'bg-amber-400' : status === 'info' ? 'bg-primary' : 'bg-white dark:bg-b-dark-light') : 'bg-white dark:bg-b-dark-light'}
--- src/pages/dashboard/shifts/shifts.page.tsx
+++ src/pages/dashboard/shifts/shifts.page.tsx
@@ -324,7 +324,7 @@ export const ShiftsPage: FunctionalComponent = () => {
           onClick={() => {
             handleViewChange(VIEW_NAME.TABLE);
           }}
-          className={`!py-3 ${currentView.value === VIEW_NAME.TABLE ? 'bg-red-300' : ''}`}
+          selected={currentView.value === VIEW_NAME.TABLE}
           icon='443'
         />
         <Button
@@ -332,7 +332,7 @@ export const ShiftsPage: FunctionalComponent = () => {
           onClick={() => {
             handleViewChange(VIEW_NAME.SCHEDULER);
           }}
-          className={`!py-3 ${currentView.value === VIEW_NAME.SCHEDULER ? 'bg-red-300' : ''}`}
+          selected={currentView.value === VIEW_NAME.SCHEDULER}
           icon='412'
         />
         <Button
@@ -340,9 +340,7 @@ export const ShiftsPage: FunctionalComponent = () => {
           onClick={() => {
             handleViewChange(VIEW_NAME.MAP);
           }}
-          className={
-            currentView.value === VIEW_NAME.MAP ? 'bg-primary-opacity' : ''
-          }
+          selected={currentView.value === VIEW_NAME.MAP}
           icon='103'
         />
 
