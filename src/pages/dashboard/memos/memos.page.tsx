a8cfbbac800324c16603bb5cc026a2f418822676
Juan Pablo Rodriguez Fernandez <juanpablorodriguezfernandez93@gmail.com>
2025-06-02 18:14:41 +0200

    fix: 🚧 remove shift layout in section

--- src/pages/dashboard/memos/memos.page.tsx
+++ src/pages/dashboard/memos/memos.page.tsx
@@ -263,7 +263,9 @@ export const MemosPage: FunctionComponent = () => {
   return (
     <Section
       className={
-        currentView.value === VIEW_NAME.CHAT ? 'flex flex-row h-[94.5vh]' : ''
+        currentView.value === VIEW_NAME.CHAT
+          ? 'flex flex-row h-[94.5vh]'
+          : 'mr-3 my-1 relative'
       }
       padding={currentView.value === VIEW_NAME.TABLE}
     >
