2260341f2cff736deb9416cc06a2ddbd018deaf1
EstebanRTryvoo <esteban.restrepo@inndi.co>
2025-07-14 15:12:41 -0500

    feat(rounds): add round points in map viewer

--- src/components/common/file/components/mapViewer.tsx
+++ src/components/common/file/components/mapViewer.tsx
@@ -6,18 +6,18 @@ const MapViewer = ({
   mapPoint,
   clickable,
 }: {
-  mapPoint?: MapPoint;
+  mapPoint?: MapPoint | MapPoint[];
   clickable?: any;
 }) => {
   const infoExpanded = (
     <MapLibrePointsMap
       name='map-points'
-      pointsRef={[mapPoint]}
+      pointsRef={Array.isArray(mapPoint) ? [...mapPoint] : mapPoint ? [mapPoint] : []}
       sendPoints={() => {}}
       disablePointSelection={true}
       center={{
-        lat: mapPoint?.position.lat || 0,
-        lng: mapPoint?.position.lng || 0,
+        lat: Array.isArray(mapPoint) ? mapPoint[0]?.position.lat || 0 : mapPoint?.position.lat || 0,
+        lng: Array.isArray(mapPoint) ? mapPoint[0]?.position.lng || 0 : mapPoint?.position.lng || 0,
       }}
       height='78vh'
       width='50%'
