a848ebf4ac72b2fc68cea36725ede20a022f9af9
EstebanRTryvoo <esteban.restrepo@inndi.co>
2025-08-06 12:47:33 -0500

    fix(compilation): fix compilation errors

--- src/pages/settings/forms/report/components/report.upsert.form.tsx
+++ src/pages/settings/forms/report/components/report.upsert.form.tsx
@@ -66,7 +66,7 @@ const ReportUpsertForm = () => {
         initialData.extraData?.projects &&
           initialData.extraData?.projects.length > 0
           ? projects.value.find(
-            (opt) => opt.value === initialData.extraData?.projects[0].value
+            (opt) => opt.value === initialData.extraData?.projects?.[0]?.value
           )
           : null,
       emails: initialData.extraData?.emails,
