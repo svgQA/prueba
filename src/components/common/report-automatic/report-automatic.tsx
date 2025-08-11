c0a4e3a2883ad342ea65f46936a8774243f8861d
EstebanRTryvoo <esteban.restrepo@inndi.co>
2025-08-11 14:39:35 -0500

    feat(disable): disabled client check

--- src/components/common/report-automatic/report-automatic.tsx
+++ src/components/common/report-automatic/report-automatic.tsx
@@ -81,7 +81,7 @@ export const ReportAutomatic = ({ modules }: ReportAutomaticProps) => {
         label: 'Cliente',
         icon: '307',
         color: 'secondary',
-        // disabled: true,
+        disabled: true,
       }
     ];
   };
