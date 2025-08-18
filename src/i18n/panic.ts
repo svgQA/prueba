fe5a3ada841b89cdd0946f40e50d3fb40cb981f9
joshua.henao <joshua.henao@inndi.co>
2025-08-18 15:18:40 -0500

    fix(memo): se soluciona bugs

--- src/i18n/panic.ts
+++ src/i18n/panic.ts
@@ -8,6 +8,9 @@ export const panic_es = {
     title: 'Alerta de pánico',
     body: '¿Quieres solucionar esta alerta de pánico?',
     success: 'Alerta de pánico solucionada',
+    title_novelty: 'Memorando',
+    body_novelty: '¿Quieres solucionar este memorando?',
+    success_novelty: 'Memorando solucionado',
   },
 };
 
@@ -21,5 +24,8 @@ export const panic_en = {
     title: 'Panic alert',
     body: 'Do you want to solve this panic alert?',
     success: 'Panic alert solved',
+    title_novelty: 'Memo',
+    body_novelty: 'Do you want to solve this memo?',
+    success_novelty: 'Memo solved',
   },
 };
