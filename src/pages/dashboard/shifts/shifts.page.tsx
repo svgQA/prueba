899a2f451cfbfd12c369fc5e162e515ece3c0b0c
Juan Pablo Rodriguez Fernandez <juanpablorodriguezfernandez93@gmail.com>
2025-08-19 21:39:46 +0200

    fix(query): 🐛 udpate filter query generator

--- src/pages/dashboard/shifts/shifts.page.tsx
+++ src/pages/dashboard/shifts/shifts.page.tsx
@@ -200,8 +200,8 @@ export const ShiftsPage: FunctionalComponent = () => {
         ShiftService.get_all({
           page: 1,
           items: 1000,
-          start: ['14/01/2024', '19/08/2025'],
-          end: ['14/01/2024', '19/08/2025'],
+          // start: ['18/08/2025', '20/08/2025'],
+          // end: ['18/08/2025', '20/08/2025'],
         }),
         ServiceService.getServicesSimpleList(),
         UserService.getListUsers(),
