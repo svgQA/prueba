b8ca360883ba9f1ce7ce90abd5139068aa4fb02e
EstebanRTryvoo <esteban.restrepo@inndi.co>
2025-11-20 16:38:38 -0500

    refactor(pqrs.general.modal): uncomment ShowFiles import and clean up commented code sections

--- src/pages/dashboard/pqrs/components/modal/pqrs-general.modal.tsx
+++ src/pages/dashboard/pqrs/components/modal/pqrs-general.modal.tsx
@@ -3,7 +3,7 @@ import { Signal } from "@preact/signals";
 import { useTranslation } from "react-i18next";
 
 import { FormattedDate } from "@/components/compose/forms";
-// import ShowFiles from '@/components/common/file/show.file';
+import ShowFiles from '@/components/common/file/show.file';
 
 import { ICPqrsRequest } from "../../utils/interface";
 
@@ -126,7 +126,7 @@ const PqrsGeneralModal = ({ pqrs }: IProps) => {
                         value={pqrs.value?.extraData.informationFile}
                     />
                 )} */}
-                {/* {pqrs.value?.resource && (
+                {pqrs.value?.resource && (
                     <div class='space-y-3'>
                         <h4 class='font-semibold text-gray-900 text-sm uppercase tracking-wide border-b pb-1'>
                             Archivos Adjuntos
@@ -135,7 +135,7 @@ const PqrsGeneralModal = ({ pqrs }: IProps) => {
                             <ShowFiles resources={pqrs.value.resource}  />
                         </div>
                     </div>
-                )} */}
+                )}
             </div>
 
             {pqrs.value?.extraData?.referencedTicketNumber && (
